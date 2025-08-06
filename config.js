e1c6854774c6a733c2292ec9dfefc40c57de22eb
FlowFalcon <safiracomel1212@gmail.com>
2025-08-06 20:57:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    botToken: '8223209229:AAGmj7Z6-dABECfLUio4WGeSHBtCE56epeY',
-    ownerId:  8178144473 // Ganti dengan ID Telegram owner bot
+    botToken: 'YOUR_TOKEN_BOT',
+    ownerId:  null // Ganti dengan ID Telegram owner bot
 };
